8fe028f4f46d46a01d36262ee35c53ec92d553b1
geojjd <martinsdeshawn@gmail.com>
2026-01-23 14:38:23 +0000

    Fix Api base

--- app/lib/api.ts
+++ app/lib/api.ts
@@ -1,7 +1,7 @@
 import axios from 'axios'
 import { supabase } from './supabase'
 
-export const API_BASE = process.env.NEXT_PUBLIC_API_BASE ?? 'http://localhost:8000'
+export const API_BASE = process.env.NEXT_PUBLIC_API_BASE! 
 
 export const api = axios.create({
     baseURL: API_BASE,
