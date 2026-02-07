6fbb7491d636529426948ed1110aeadd19c8f02f
geojjd <martinsdeshawn@gmail.com>
2026-02-07 23:18:51 +0000

    Cool

--- app/lib/plan.ts
+++ app/lib/plan.ts
@@ -1,23 +1,36 @@
 export type PlanName = "free" | "starter" | "pro";
 
-export const PLANS: Record<PlanName, {
-    stemExportAllowed: any;
+export const PLANS: Record<
+  PlanName,
+  {
+    stemExportAllowed: boolean;
     exportsPerMonth: number;
     aiGenerationsPerMonth: number;
     name: string;
     monthlyPrice: number;
-}> = {
-    free: {
-        name: "Free",
-        monthlyPrice: 0,
+  }
+> = {
+  free: {
+    name: "Free",
+    monthlyPrice: 0,
+    stemExportAllowed: false,
+    exportsPerMonth: 5,
+    aiGenerationsPerMonth: 10,
+  },
 
-    },
-    starter: {
-        name: "Starter",
-        monthlyPrice: 29,
-    },
-    pro: {
-        name: "Pro",
-        monthlyPrice: 29,
-    }
+  starter: {
+    name: "Starter",
+    monthlyPrice: 29,
+    stemExportAllowed: true,
+    exportsPerMonth: 50,
+    aiGenerationsPerMonth: 200,
+  },
+
+  pro: {
+    name: "Pro",
+    monthlyPrice: 49,
+    stemExportAllowed: true,
+    exportsPerMonth: 500,
+    aiGenerationsPerMonth: 2000,
+  },
 };
